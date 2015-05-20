a71776c417efb0ef813e10d9d35e9179cf2a2020
Pete Abred <pete@abred.de>
2015-05-21 01:28:37 +0200

    add compiler flag for return address protection

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -3409,6 +3409,9 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   else if (Args.hasArg(options::OPT_fprofile_instr_use))
     CmdArgs.push_back("-fprofile-instr-use=pgo-data");
 
+  if (Args.hasArg(options::OPT_frepl_return))
+    CmdArgs.push_back("-frepl-return");
+
   if (Args.hasArg(options::OPT_ftest_coverage) ||
       Args.hasArg(options::OPT_coverage))
     CmdArgs.push_back("-femit-coverage-notes");
--- lib/Frontend/CompilerInvocation.cpp
+++ lib/Frontend/CompilerInvocation.cpp
@@ -1591,6 +1591,7 @@ static void ParseLangArgs(LangOptions &Opts, ArgList &Args, InputKind IK,
   Opts.NativeHalfType |= Args.hasArg(OPT_fnative_half_type);
   Opts.HalfArgsAndReturns = Args.hasArg(OPT_fallow_half_arguments_and_returns);
   Opts.GNUAsm = !Args.hasArg(OPT_fno_gnu_inline_asm);
+  Opts.ReplReturn = Args.hasArg(OPT_frepl_return);
 
   if (!Opts.CurrentModule.empty() && !Opts.ImplementationOfModule.empty() &&
       Opts.CurrentModule != Opts.ImplementationOfModule) {
